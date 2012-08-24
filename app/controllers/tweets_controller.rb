90a1527309d2c357dc97cf3b42e04d3d6e3bfe36
Jessie Young <jessie.a.young@gmail.com>
2012-08-24 15:08:21 -0700

    generated devise views

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,26 @@
+class TweetsController < ApplicationController
+  before_filter :authenticate_user!, :except => [:index]
+  
+  def index
+    @tweets = Tweet.all
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+  
+  def create
+    @tweet = Tweet.new(params[:tweet])
+    @tweet.user = current_user
+      if @tweet.save
+        redirect_to @tweet
+      else
+        render 'new'
+      end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+end
---
+++ app/models/tweet.rb
@@ -0,0 +1,4 @@
+class Tweet < ActiveRecord::Base
+  attr_accessible :description, :user_id
+  belongs_to :user
+end
